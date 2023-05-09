995a69fc198d3748df64e7bbdb6fb801eeaa002f
shankhalam <shankhalamh@gmail.com>
2023-05-09 21:40:22 +0530

    Create app titles to portfolio.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout='wide')
 
@@ -17,3 +18,20 @@ with col2:
     Bootstrap.
     """
     st.info(content)
+
+content2 = """Below you can find some Apps which i made from Python. Feel free to contact for more information about 
+any of the apps."""
+st.write(content2)
+
+df = pandas.read_csv('data.csv', sep=';')
+
+col3, col4 = st.columns(2)
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row['title'])
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+
+
