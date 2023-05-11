98b0e3742442ae58b97f177cbff0098db793d150
shankhalam <shankhalamh@gmail.com>
2023-05-11 22:48:55 +0530

    Send mail action implemented.

--- pages/Contact Us.py
+++ pages/Contact Us.py
@@ -1,8 +1,19 @@
 import streamlit as st
+from send_mail import send_mail
 
 st.header("Contact Us")
 
 with st.form(key='email_form'):
     email = st.text_input("Enter your Email")
-    message = st.text_area("Enter your Message")
+    raw_message = st.text_area("Enter your Message")
+    message = f"""\
+Subject: New Message from Portfolio Website
+
+From: {email}
+{raw_message}    
+"""
     button = st.form_submit_button("Submit")
+
+    if button:
+        send_mail(message)
+        st.info("Your message send successfully.Get back you soon.")
